5292bd19d8d05758765f7d3b8ca532f595dc7c6b
alice <82486617+alice-l2@users.noreply.github.com>
2022-07-28 21:13:00 +0900

    Chore : 불필요한 theme 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css"
 import "../styles/tailwind.css"
+import "react-datepicker/dist/react-datepicker.css"
 import type {AppProps} from "next/app"
 
 function MyApp({Component, pageProps}: AppProps) {
